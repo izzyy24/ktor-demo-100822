e059284d99ee413f16ff1e98b16a63e7e50a0f82
izzyy24 <110359259+izzyy24@users.noreply.github.com>
2022-08-10 11:14:16 +1000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.6.21"
-}
-
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+val ktorVersion = "2.0.3"
+val logbackVersion = "1.2.11"
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+}
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
 repositories {
     mavenCentral()
 }
-
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 }
-
 tasks.test {
     useJUnitPlatform()
 }
-
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
